9b8184dcf7c09a4767a5b6e202099ca55fec4f98
Sharuna-mk <sharunamk3@gmail.com>
2026-03-22 18:13:05 +0530

    langnest

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -7,7 +7,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://langnest-backend-xozj.onrender.com", {
       transports: ["websocket"],
     });
 
